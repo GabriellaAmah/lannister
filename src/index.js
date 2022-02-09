20f1205240d695e0e9dda6afda04b2cfcf00863d
GabriellaAmah <gabbieamah49@gmail.com>
2022-02-09 12:20:05 +0100

    added cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express, {json} from 'express'
 import dotenv from 'dotenv'
 import router from './routes'
+import cors from 'cors'
 
 async function startServer() {
     dotenv.config()
 
     const app = express()
+
+    app.use(cors())
 
     app.use(json())
     app.use(router)
